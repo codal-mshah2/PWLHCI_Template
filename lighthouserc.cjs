ee49bd57c7b144c90f5d2f8c6b92f1577dc2cc2e
codal-mshah2 <mshah2@codal.com>
2026-02-11 15:55:09 +0530

    ⚡ UP: Select profile for chrome to reuse

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,13 +1,11 @@
 module.exports = {
   ci: {
     collect: {
-      url: [
-        "https://7.qa.tacorosa.club/",
-        "https://7.qa.tacorosa.club/signup",
-        "https://7.qa.tacorosa.club/top-models",
-      ],
+      url: ["https://7.qa.tacorosa.club/"],
       numberOfRuns: 1,
-      chromeFlags: "--user-data-dir=./pw-user-data",
+
+      chromeFlags: ["--user-data-dir=./pw-user-data", "--profile-directory=Default"],
+
       settings: {
         disableStorageReset: true,
         formFactor: "desktop",
@@ -25,12 +23,5 @@ module.exports = {
       serverBaseUrl: "http://localhost:9001",
       token: "59363e33-267d-49fd-a4b3-5ddddc5a440e",
     },
-
-    // Optional – you can add later
-    // assert: {
-    //   assertions: {
-    //     'categories:performance': ['warn', { minScore: 0.6 }],
-    //   },
-    // },
   },
 };
