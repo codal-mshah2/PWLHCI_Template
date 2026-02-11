cdbd6e2c7a2c897d27ac34953b257e9f910f16b4
codal-mshah2 <mshah2@codal.com>
2026-02-11 15:39:05 +0530

    ⚡ UP:  Add collect in lighthouserc to let lighthouse create the report

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,17 +1,35 @@
 module.exports = {
   ci: {
+    collect: {
+      url: [
+        "https://7.qa.tacorosa.club/",
+        "https://7.qa.tacorosa.club/signup",
+        "https://7.qa.tacorosa.club/top-models",
+      ],
+      numberOfRuns: 1,
+      chromeFlags: "--user-data-dir=./pw-user-data",
+      settings: {
+        formFactor: "desktop",
+        screenEmulation: {
+          mobile: false,
+          width: 1350,
+          height: 940,
+          deviceScaleFactor: 1,
+        },
+      },
+    },
+
     upload: {
       target: "lhci",
       serverBaseUrl: "http://localhost:9001",
       token: "59363e33-267d-49fd-a4b3-5ddddc5a440e",
     },
 
-    server: {
-      storage: {
-        storageMethod: "sql",
-        sqlDialect: "sqlite",
-        sqlDatabasePath: "./db.sql",
-      },
-    },
+    // Optional – you can add later
+    // assert: {
+    //   assertions: {
+    //     'categories:performance': ['warn', { minScore: 0.6 }],
+    //   },
+    // },
   },
 };
