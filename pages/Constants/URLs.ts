fbc4a886fe0581b0f872d51d6ffe1a8c59b481fc
codal-mshah2 <mshah2@codal.com>
2026-02-12 15:40:25 +0530

    ⚡ UP: Update URLS

--- pages/Constants/URLs.ts
+++ pages/Constants/URLs.ts
@@ -1,5 +1,5 @@
 export const urlsToAudit = [
   "https://7.qa.tacorosa.club/",
-  "https://7.qa.tacorosa.club/signup",
   "https://7.qa.tacorosa.club/top-models",
+  "https://7.qa.tacorosa.club/trending",
 ];
