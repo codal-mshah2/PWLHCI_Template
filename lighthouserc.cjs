82b01672a9e943d6abb827215e50f93d4fd82876
codal-mshah2 <mshah2@codal.com>
2026-02-12 13:21:01 +0530

    ⚡ UP: Make the collect simple as chromeflags do not work

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -3,27 +3,19 @@ module.exports = {
     collect: {
       url: ["https://7.qa.tacorosa.club/"],
       numberOfRuns: 1,
-
-      chromeFlags: [
-        "--user-data-dir=C:/Users/mshah/AppData/Local/Google/Chrome/User Data",
-        "--profile-directory=Default",
-      ],
-      settings: {
-        disableStorageReset: true,
-        formFactor: "desktop",
-        screenEmulation: {
-          mobile: false,
-          width: 1350,
-          height: 940,
-          deviceScaleFactor: 1,
-        },
-      },
     },
 
     upload: {
       target: "lhci",
       serverBaseUrl: "http://localhost:9001",
       token: "59363e33-267d-49fd-a4b3-5ddddc5a440e",
+    },
+    server: {
+      storage: {
+        storageMethod: "sql",
+        sqlDialect: "sqlite",
+        sqlDatabasePath: "./db.sql",
+      },
     },
   },
 };
